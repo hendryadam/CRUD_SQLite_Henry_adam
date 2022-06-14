0f44fb952df1dc17feabf25bbccf19bdeed09422
HENRY ADAM HIDAYAT <2011500085@mahasiswa.atmaluhur.ac.id>
2022-06-14 21:20:19 +0700

    Initial commit

---
+++ app/src/main/java/com/example/crudsqlite_henryadam/Item.kt
@@ -0,0 +1,11 @@
+package com.example.crudsqlite_henryadam
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Item : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.item)
+    }
+}
